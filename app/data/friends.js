362f56a3783a7092a2cbe52402e9e7c66f5d7ce9
Dillon Schultz <dillonschultz93@gmail.com>
2018-02-05 11:40:11 -0700

    Added dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+// file that contains an array of objects that represent people and their 
+// responses to the questions being asked on servey.html
+
+let users = [
+  {
+    name: 'John Doe', 
+    photo: 'http://www.google.com',
+    scores: [
+      2,
+      4, 
+      3,
+      1,
+      3,
+      5,
+      2,
+      1,
+      5,
+      2
+    ]
+  },
+  {
+    name: 'Namey McNameface',
+    photo: 'http://www.google.com',
+    scores: [
+      4,
+      2,
+      3,
+      5,
+      3,
+      1,
+      4,
+      5,
+      1,
+      4
+    ]
+  },
+  {
+    name: 'Ashley Gotobed',
+    photo: 'http://www.google.com',
+    scores: [
+      1,
+      3,
+      2,
+      5,
+      2,
+      4,
+      1,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: 'David Idonthavealastname',
+    photo: 'http://www.google.com',
+    scores: [
+      5,
+      3,
+      4,
+      1,
+      4,
+      2,
+      3,
+      5,
+      1,
+      2
+    ]
+  },
+  {
+    name: 'Dillon Schultz',
+    photo: '#',
+    scores: [
+      4,
+      3,
+      3,
+      4,
+      1,
+      4,
+      3,
+      4,
+      5,
+      2
+    ]
+  }
+]
+
+// export the data
+module.exports = users
